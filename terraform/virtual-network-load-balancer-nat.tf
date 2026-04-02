8df8c67003a45662eebafedc8d747361a778573e
Himesh Patel <himesh.patel@version1.com>
2026-04-02 12:33:24 +0100

    change

--- terraform/virtual-network-load-balancer-nat.tf
+++ terraform/virtual-network-load-balancer-nat.tf
@@ -1,7 +1,7 @@
 
 resource "azurerm_lb_nat_rule" "wowza" {
   for_each = var.environment == "stg" ? {} : local.lb-rules
-  
+
   resource_group_name            = azurerm_resource_group.rg.name
   loadbalancer_id                = azurerm_lb.cvp.id
   name                           = "${each.key}-nat"
