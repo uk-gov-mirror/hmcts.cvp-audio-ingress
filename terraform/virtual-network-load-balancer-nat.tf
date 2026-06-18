2730079b2f8ffc78c5d766ffbc46c691b1e540f3
himesh <himesh.patel@hmcts.net>
2026-06-18 19:31:50 +0100

    nat

--- terraform/virtual-network-load-balancer-nat.tf
+++ terraform/virtual-network-load-balancer-nat.tf
@@ -1,14 +1,14 @@
 
-# resource "azurerm_lb_nat_rule" "wowza" {
-#   for_each = var.environment == "stg" ? {} : local.lb-rules
+resource "azurerm_lb_nat_rule" "wowza" {
+  for_each = var.environment == "stg" ? {} : local.lb-rules
 
-#   resource_group_name            = azurerm_resource_group.rg.name
-#   loadbalancer_id                = azurerm_lb.cvp.id
-#   name                           = "${each.key}-nat"
-#   protocol                       = each.value.protocol
-#   frontend_port_start            = each.value.frontend_port + 1
-#   frontend_port_end              = each.value.frontend_port + 2
-#   backend_port                   = each.value.backend_port
-#   backend_address_pool_id        = azurerm_lb_backend_address_pool.wowza.id
-#   frontend_ip_configuration_name = "PrivateIPAddress"
-# }
+  resource_group_name            = azurerm_resource_group.rg.name
+  loadbalancer_id                = azurerm_lb.cvp.id
+  name                           = "${each.key}-nat"
+  protocol                       = each.value.protocol
+  frontend_port_start            = each.value.frontend_port + 1
+  frontend_port_end              = each.value.frontend_port + 2
+  backend_port                   = each.value.backend_port
+  backend_address_pool_id        = azurerm_lb_backend_address_pool.wowza.id
+  frontend_ip_configuration_name = "PrivateIPAddress"
+}
